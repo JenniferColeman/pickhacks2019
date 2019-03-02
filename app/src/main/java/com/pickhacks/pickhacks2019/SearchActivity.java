ea31a53167d5d22d1bb82b4c4ef255e0dd1d25cd
Connor J. Toth <tothconnor@gmail.com>
2019-03-02 17:50:42 -0600

    wE GOT CARROTs!

--- app/src/main/java/com/pickhacks/pickhacks2019/SearchActivity.java
+++ app/src/main/java/com/pickhacks/pickhacks2019/SearchActivity.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private ArrayList<SearchItem> mSearchList;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +26,22 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mSearchList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            mSearchList.add(new SearchItem(
+                    R.drawable.food,
+                    "This is a brief description of the thing.",
+                    false,
+                    "3 months",
+                    "Carrot"));
+        }
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(false);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new SearchItemAdapter(mSearchList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
+
 }
